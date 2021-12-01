33ce802edb6a09cb4ce6aae62129dd0372f018df
mitsumaru3291 <morimitsu8428@gmail.com>
2021-12-01 16:54:22 +0900

    update display_test.py

--- display_atlas.py
+++ display_atlas.py
@@ -1,5 +1,4 @@
 import pinocchio
-from pinocchio.robot_wrapper import RobotWrapper
 import numpy as np
 from utils.Visualizer import Visualizer
 from os.path import abspath
@@ -14,19 +13,36 @@ if __name__ == "__main__":
 
     ## Give random joint angle series
     root = pinocchio.JointModelFreeFlyer()
-    model = pinocchio.buildModelFromUrdf(atlas_urdf, root) # Generate iiwa14 model (7DOF) from URDF
+    model = pinocchio.buildModelFromUrdf(atlas_urdf, root) # Generate atlas model from URDF
+    """
+    print(model.getFrameId('r_arm_wry2'))
+    print(model.getFrameId('l_arm_wry2'))
+    print(model.getFrameId('r_hand'))
+    print(model.getFrameId('l_hand'))
+    print(model.getFrameId('r_ufarm'))
+    print(model.getFrameId('l_uarm'))
+    print(model.getFrameId('r_talus'))
+    print(model.getFrameId('l_talus'))
+    """
+    for i in range(len(model.joints)):
+        #print(f"Number : {i}")
+        print(model.names[i])
+        print(model.joints[i])
+    """
     for i in range(len(model.frames)):
         print(f"Number : {i}")
-        print(model.frames[i])
-
+        print(model.frames[i].name)
     """
+
     q_traj = []
-    q_traj.append(0.1 * pinocchio.randomConfiguration(model)) # Generate random joint angle
+    q0 = pinocchio.normalize(model, np.zeros(model.nq))
+    q0[2] = 1.0
+    q_traj = [q0]
     for i in range(N):
-        v = 0.05 * np.random.rand(7)
+        v = 0.05 * np.random.rand(model.nv)
+        v = np.zeros(model.nv)
         q_traj.append(pinocchio.integrate(model, q_traj[-1], v)) 
 
     ## Visualize
-    visualizer = Visualizer(atlas_urdf) # Create Visualizer object
+    visualizer = Visualizer(atlas_urdf)
     visualizer.display_meshcat(dt=dt, q_traj=q_traj)
-    """
